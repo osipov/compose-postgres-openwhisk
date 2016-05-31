54215c36b283037a9104c40b9458ddd9a0b9b23e
osipov <osipov@gmail.com>
2016-05-31 16:40:35 -0400

    fix license

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+// Licensed under the Apache License. See footer for details.
 var util = require('util')
 var bodyParser = require('body-parser')
 var express = require('express')
@@ -77,3 +78,18 @@ app.use(function(err, req, res, next) {
 });
 console.log('launching an OpenWhisk Docker-based action implementation...');
 service.start(app);
+//-------------------------------------------------------------------------------
+// Copyright IBM Corp. 2016
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-------------------------------------------------------------------------------
